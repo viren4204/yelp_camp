9be98adaefd5fc09b43858ddb45d04f6c9452ca7
viren4204 <45611825+viren4204@users.noreply.github.com>
2020-06-01 21:37:58 +0530

    put campgroundSchema in a diff. module/file

--- app.js
+++ app.js
@@ -2,20 +2,12 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost/yelp_camp_01');
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
-
-//schema setup
-var campgroundSchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create(
 //   {
