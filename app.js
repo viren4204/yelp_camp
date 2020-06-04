3c19c886e28dd3926bd17773a1f76400784dea0c
viren4204 <45611825+viren4204@users.noreply.github.com>
2020-06-04 22:48:31 +0530

    reverted back

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Campground = require('./models/campground');
-var seedDB = require("./seeds");
-var comment = require('./models/comment');
+var seedDB = require("./seeds")
 
 mongoose.connect('mongodb://localhost/yelp_camp_01');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -75,32 +74,9 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
     } else {
       res.render("comments/new", {campground: campground});
     }
-  });
+  })
 });
 
-app.post("/campgrounds/:id/comments", function(req, res){
-  //lookup campground using // ID
-  campground.findById(req.params.id, function(err, campground){
-    if (err) {
-      console.log(err);
-      res.redirect('/campgrounds');
-    } else {
-        Comment.create(req.body.comment, function(err, comment){
-          if (err) {
-            console.log(err);
-          } else {
-            campground.comments.push(comment);
-            campground.save();
-            res.redirect("/campgdrounds/" + campground._id);
-          }
-        })
-    }
-  });
-  //create new comment
-  //connect new comment to campground
-  //  redirect to campground show page
-
-});
 
 app.listen(3000, function(){
   console.log('yelpCamp server has started');
